404850bd051da3d2e0e90d487fdc9c2452c573f3
joogps <joaogabrielps13@hotmail.com>
2019-01-29 22:23:50 -0200

    Bug fixes

--- script.js
+++ script.js
@@ -6,35 +6,41 @@ $(function(){
 		$("#flag").append(slice);
 	}
 
-	const letters = "LGBTT2QQIAAPAGBGP";
-	for(let l of letters) {
-		let box = $("<div>").addClass("letter-box");
-		let letter = $("<div>").addClass("letter").html(l);
-		box.append(letter);
-		$("#acronym").append(box);
-	}
+	let smallestWidth;
+	$.getJSON("descriptions.json", function(data) {
+		const letters = "LGBTT2QQIAAPAGBGP";
+		for (let i = 0; i < letters.length; i++) {
+			let box = $("<div>").addClass("letter-box");
+			let letter = $("<div>").addClass("letter").html(letters[i]);
+			let contents = $("<div>").addClass("contents");
+
+			contents.append($("<div>").html(data.order[i]["meaning"]).addClass("title"));
+			contents.append($("<div>").html(data.order[i]["description"]).addClass("description"));
+
+			box.append(letter);
+			box.append(contents);
+
+			$("#acronym").append(box);
+
+			box.width(box.width()*1.3);
+			box.height(box.height());
+		}
+	});
 
 	$.getJSON("descriptions.json", function(data) {
 		$(".letter-box").each(function(i) {
 			let box = $(this);
 			box.mouseenter(function() {
 				box.find(".letter").transition({"font-size": "65vh", opacity: "0"}, 200, function(){
-					let contents = $("<div>").addClass("contents");
-
-					contents.append($("<div>").html(data.order[i]["meaning"]).addClass("title"));
-					contents.append($("<div>").html(data.order[i]["description"]).addClass("description"));
-
-					$(this).css("display", "hidden");
-					box.append(contents);
-
-					contents.css({"transform": "perspective(1px) scale(0.5) translateY(-50%)", opacity: "0"});
-					contents.transition({"scale": "1", opacity: "1"}, 200);
+					$(this).css("display", "none");
+					box.find(".contents").css("display", "block");
+					box.find(".contents").transition({scale: "1", opacity: "1"}, 200);
 				});
 			});
 
 			box.mouseleave(function() {
 				box.find(".contents").transition({"scale": "0.5", opacity: "0"}, 200, function(){
-					$(this).css("display", "hidden");
+					$(this).css("display", "none");
 					box.find(".letter").css("display", "inline-block");
 					box.find(".letter").transition({"font-size": "80vh", opacity: "1"}, 200);
 				});
