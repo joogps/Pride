8de1ef77b8b82bccf49fa0c25357702158ce936c
joogps <joaogabrielps13@hotmail.com>
2019-02-04 21:49:08 -0200

    Added an interactive tutorial to the webpage

--- javascript/script.js
+++ javascript/script.js
@@ -1,91 +1,149 @@
 $(function(){
-	$.getJSON("data/letters.json", function(data) {
-		for (let l of data.letters) {
-			let box = $("<a>").addClass("letter-box");
-			let letter = $("<div>").addClass("letter").html(l.letter);
-			let contents = $("<div>").addClass("letter-contents");
+	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+		$("#bubble1").html("Touch a letter to see its meaning. Do it again to learn more about it.");
+		$("#bubble2").html("Touch the rainbow slices to view details about the webpage.");
+	}
+	else{
+		$("#bubble1").html("Hover your mouse cursor onto a letter to see its meaning. Click to learn more about it.");
+		$("#bubble2").html("Hover your mouse cursor onto the rainbow slices to view details about the webpage.");
+	}
 
-			box.attr("href", l.wikipedia);
-
-			contents.append($("<div>").html(l.meaning).addClass("letter-contents-title"));
-			contents.append($("<div>").html(l.description).addClass("letter-contents-description"));
-
-			box.append(letter);
-			box.append(contents);
-
-			box.click(function(event) {
-				event.preventDefault();
-			});
-
-			box.mouseenter(function() {
-				let box = $(this);
-				box.find(".letter").stop().animate({"font-size": "65vh", opacity: "0"}, 200, function(){
-					$(this).css("display", "none");
-					box.find(".letter-contents").css("display", "block");
-					box.find(".letter-contents").stop().transition({scale: "1", opacity: "1"}, 200, function() {
-						box.off("click");
-					});
-				});
-			});
-
-			box.mouseleave(function() {
-				box.find(".letter-contents").stop().transition({scale: "0.5", opacity: "0"}, 200, function(){
-					$(this).css("display", "none");
-					box.find(".letter").css("display", "inline-block");
-					box.find(".letter").stop().animate({"font-size": "80vh", opacity: "1"}, 200, function() {
-						box.click(function(event) {
-							event.preventDefault();
+	createLetters();
+	$("#acronym").animate({opacity: "1"}, function(){
+		$("#bubble1").css("display", "block");
+		$("#bubble1").animate({opacity: "1"}, function() {
+			$("body").click(function() {
+				$(window).off("click");
+				$("#bubble1").animate({opacity: "0"},function(){
+					$("#bubble1").css("display", "none");
+					createSlices();
+					$("#acronym").animate({opacity: "0"}, function() {
+						$("#acronym").css("display", "none");
+						$("#bubble2").css("display", "block");
+						$("#bubble2").animate({opacity: "1"}, function() {
+							$(window).click(function() {
+								$("body").off("click");
+								$("#bubble2").animate({opacity: "0"}, function(){
+									listenToSlices();
+									$("#bubble2").css("display", "none");
+									$("#acronym").css("display", "flex");
+									$("#acronym").animate({opacity: "1"}, function() {
+										listenToLetters();
+									});
+								});
+							});
 						});
 					});
 				});
 			});
-
-			$("#acronym").append(box);
-
-			box.width(box.width()*1.3);
-			box.height(box.height());
-		}
-
-		$(window).resize(function() {
-			$(".letter-box").width("auto");
-			$(".letter-box").height("auto");
-			$(".letter-box").width($(".letter-box").width()*1.3);
-			$(".letter-box").height($(".letter-box").height());
 		});
 	});
 
+	function createLetters() {
+		$.getJSON("data/letters.json", function(data) {
+			for (let l of data.letters) {
+				let box = $("<a>").addClass("letter-box");
+				let letter = $("<div>").addClass("letter").html(l.letter);
+				let contents = $("<div>").addClass("letter-contents");
 
+				box.attr("href", l.wikipedia);
+				box.click(function(event) {
+					event.preventDefault();
+				});
+				box.css("cursor", "default");
 
-	$.getJSON("data/slices.json", function(data) {
-		for(let s of data.slices) {
-			let slice = $("<div>").addClass("slice").css("background-color", s.color);
-			let contents = $("<div>").addClass("slice-contents");
-			contents.append($("<div>").addClass("slice-text").html(s.html));
+				contents.append($("<div>").html(l.meaning).addClass("letter-contents-title"));
+				contents.append($("<div>").html(l.description).addClass("letter-contents-description"));
+				box.append(contents);
+				box.append(letter);
 
-			contents.find("a").click(function(event) {
-				event.preventDefault();
+				$("#acronym").append(box);
+
+				box.width(box.width()*1.3);
+				box.height(box.height());
+			}
+
+			$(window).resize(function() {
+				$(".letter-box").width("auto");
+				$(".letter-box").height("auto");
+				$(".letter-box").width($(".letter-box").width()*1.3);
+				$(".letter-box").height($(".letter-box").height());
 			});
+		});
+	}
 
-			slice.delay(Math.random()*800).animate({left: "0px"}, 300+Math.random()*1600, function() {
-				slice.mouseenter(function() {
-					slice.stop().animate({width: "30vw"}, 300);
-					contents.stop().animate({opacity: 1}, 300, function() {
-						contents.find("a").off("click");
+	function listenToLetters() {
+		$(".letter-box").mouseenter(function() {
+			let box = $(this);
+			box.find(".letter").stop().animate({"font-size": "65vh", opacity: "0"}, 200, function(){
+				$(this).css("display", "none");
+				box.find(".letter-contents").css("display", "block");
+				box.find(".letter-contents").stop().transition({scale: "1", opacity: "1"}, 200, function() {
+					box.off("click");
+				});
+			});
+		});
+
+		$(".letter-box").mouseleave(function() {
+			let box = $(this);
+			box.find(".letter-contents").stop().transition({scale: "0.5", opacity: "0"}, 200, function(){
+				$(this).css("display", "none");
+				box.find(".letter").css("display", "inline-block");
+				box.find(".letter").stop().animate({"font-size": "80vh", opacity: "1"}, 200, function() {
+					box.click(function(event) {
+						event.preventDefault();
 					});
 				});
+			});
+		});
 
-				slice.mouseleave(function() {
-					slice.stop().animate({width: "20vw"}, 300);
-					contents.stop().animate({opacity: 0}, 300, function() {
-						contents.find("a").click(function(event) {
-							event.preventDefault();
-						});
-					});
+		$(".letter-box").css("cursor", "pointer");
+	}
+
+	function createSlices() {
+		$.getJSON("data/slices.json", function(data) {
+			for(let s of data.slices) {
+				let slice = $("<div>").addClass("slice").css("background-color", s.color);
+				let contents = $("<div>").addClass("slice-contents");
+				contents.append($("<div>").addClass("slice-text").html(s.html));
+
+				contents.css("cursor", "default");
+				contents.find("a").css("cursor", "default");
+				contents.find("a").click(function(event) {
+					event.preventDefault();
+				});
+
+				slice.delay(Math.random()*800).animate({left: "0px"}, 300+Math.random()*1600);
+
+				slice.append(contents);
+				$("#flag").append(slice);
+			}
+		});
+	}
+
+	function listenToSlices() {
+		$(".slice").mouseenter(function() {
+			let slice = $(this);
+			let contents = slice.find(".slice-contents");
+			slice.stop().animate({width: "30vw"}, 300);
+			contents.stop().animate({opacity: 1}, 300, function() {
+				contents.css("cursor", "text");
+				contents.find("a").css("cursor", "pointer");
+				contents.find("a").off("click");
+			});
+		});
+
+		$(".slice").mouseleave(function() {
+			let slice = $(this);
+			let contents = slice.find(".slice-contents");
+			slice.stop().animate({width: "20vw"}, 300);
+			contents.stop().animate({opacity: 0}, 300, function() {
+				contents.css("cursor", "default");
+				contents.find("a").css("cursor", "default");
+				contents.find("a").click(function(event) {
+					event.preventDefault();
 				});
 			});
-
-			slice.append(contents);
-			$("#flag").append(slice);
-		}
-	});
+		});
+	}
 });
